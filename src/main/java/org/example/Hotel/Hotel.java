dc9691d745aee5ac157f77f63544ed4bc9e49a9a
siva jyothika oguri <sivajyothika8@gmail.com>
2024-06-26 18:03:32 +0530

    Created BoilerPlate for the project

---
+++ src/main/java/org/example/Hotel/Hotel.java
@@ -0,0 +1,96 @@
+package org.example.Hotel;
+
+import java.util.List;
+
+public class Hotel {
+    String city;
+    String state;
+    String country;
+    String uid;
+    List<String> facilities;
+    List<String> roomFacilities;
+    int reviewCount;
+    float reviewScore;
+    String hotelName;
+    String hotelType;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public List<String> getFacilities() {
+        return facilities;
+    }
+
+    public void setFacilities(List<String> facilities) {
+        this.facilities = facilities;
+    }
+
+    public List<String> getRoomFacilities() {
+        return roomFacilities;
+    }
+
+    public void setRoomFacilities(List<String> roomFacilities) {
+        this.roomFacilities = roomFacilities;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public float getReviewScore() {
+        return reviewScore;
+    }
+
+    public void setReviewScore(float reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getHotelType() {
+        return hotelType;
+    }
+
+    public void setHoterType(String hotelType) {
+        this.hotelType = hotelType;
+    }
+}
---
+++ src/main/java/org/example/Hotel/HotelsIndexer.java
@@ -0,0 +1,4 @@
+package org.example.Hotel;
+
+public class HotelsIndexer {
+}
